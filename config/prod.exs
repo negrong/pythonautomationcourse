4c0c3d757c2c3e3d44b122bedf0aebeb7adb1ca7
negrong <69174899+negrong@users.noreply.github.com>
2021-08-29 13:30:17 +0000

    v2.5.6

--- config/prod.exs
+++ config/prod.exs
@@ -13,4 +13,5 @@ config :SmartBank, SmartBankWeb.Endpoint,
   url: [scheme: "https", host: "smart-bank", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   server: true,
-  cache_static_manifest: "priv
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  ren
