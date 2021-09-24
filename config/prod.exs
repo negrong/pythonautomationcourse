7faa5c75bb0b9b255a97215e3b0522eaf6011cc5
negrong <69174899+negrong@users.noreply.github.com>
2021-09-24 14:18:58 +0000

    update minor version

--- config/prod.exs
+++ config/prod.exs
@@ -56,4 +56,6 @@ config :logger, level: :info
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant en
+# to start each relevant endpoint:
+#
+#     config :SmartBank,
