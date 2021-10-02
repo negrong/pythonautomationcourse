6f9c46fecf5273225d555e09978839690709b222
negrong <69174899+negrong@users.noreply.github.com>
2021-10-02 07:55:06 +0000

    SLING-3027 Improve Launchpad Karaf Features

--- config/test.exs
+++ config/test.exs
@@ -9,4 +9,5 @@ config :SmartBank, SmartBank.Repo,
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
-# you can enable the s
+# you can enable the server option below.
+config :Sm
