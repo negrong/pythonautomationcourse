8822dcb61a219bb5c9918f782e1260c6a4bad509
negrong <69174899+negrong@users.noreply.github.com>
2021-10-02 18:28:14 +0000

    Added sounds

--- config/test.exs
+++ config/test.exs
@@ -11,3 +11,5 @@ config :SmartBank, SmartBank.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :SmartBank, SmartBankWeb.Endpoint,
+  http: [port: 4002],
+  server: fa
