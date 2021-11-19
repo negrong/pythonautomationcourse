a07ef1c5b79180f214e81f5195a8cd9f163e97bb
negrong <69174899+negrong@users.noreply.github.com>
2021-11-19 12:49:02 +0000

    added podspec

--- lib/smart_bank/application.ex
+++ lib/smart_bank/application.ex
@@ -25,4 +25,4 @@ defmodule SmartBank.Application do
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
   def config_change(changed, _new, removed) do
-    SmartBa
+    SmartBankWeb.Endpoint.config_change(changed
