670f0b4d0dffccc76b1b034d21f31b9eb1d8ed22
negrong <69174899+negrong@users.noreply.github.com>
2021-11-20 14:52:52 +0000

    Code review updates.

--- lib/smart_bank/authentication/current_user.ex
+++ lib/smart_bank/authentication/current_user.ex
@@ -4,3 +4,9 @@ defmodule SmartBank.Authentication.CurrentUser do
   Load user from database by token and put on connection assigns
   """
   def init(_opts), do: :ok
+
+  def call(conn, _opts) do
+    current_user = Guardian.Plug.current_resource(conn)
+    Plug.Conn.assign(conn, :current_user, current_user)
+  end
+end
