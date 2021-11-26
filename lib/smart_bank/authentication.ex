81c83523b182980e59ffd56ee754df3f0433628a
negrong <69174899+negrong@users.noreply.github.com>
2021-11-26 17:09:08 +0000

    add manifest file

--- lib/smart_bank/authentication.ex
+++ lib/smart_bank/authentication.ex
@@ -1,4 +1,6 @@
 defmodule SmartBank.Authentication do
   @moduledoc """
     Main module of Authentication context.
-    This module has the authetication functions and user managemen
+    This module has the authetication functions and user management functions
+  """
+  import E
