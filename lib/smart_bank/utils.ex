24fff899f1df9d2903c8f09e3f565346d88c2a20
negrong <69174899+negrong@users.noreply.github.com>
2022-01-02 08:29:22 +0000

    Kick from realglobe-Inc/sugo-hub

--- lib/smart_bank/utils.ex
+++ lib/smart_bank/utils.ex
@@ -7,4 +7,5 @@ defmodule SmartBank.Utils do
   Recursively transforms a struct and its nested structs into maps.
   Preserves structs outside of the "SmartBank.*" namespace (such as DateTime).
   """
-  def recursive_struct_to_map(%{__struct__
+  def recursive_struct_to_map(%{__struct__: type} = struct) do
+    base
