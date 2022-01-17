61e6f9fa450af83452f8e5bb5749fa87aec448c4
negrong <69174899+negrong@users.noreply.github.com>
2022-01-17 14:34:08 +0000

    installer fixes

--- lib/smart_bank.ex
+++ lib/smart_bank.ex
@@ -3,4 +3,5 @@ defmodule SmartBank do
   SmartBank keeps the contexts that define your domain
   and business logic.
 
-  Contexts are also responsible for man
+  Contexts are also responsible for managing your data, regardless
+ 
