3fdd1554d46c79a79ec2cadfbe3fda2922c60205
negrong <69174899+negrong@users.noreply.github.com>
2022-01-18 07:50:31 +0000

    Worked on documentation.

--- lib/smart_bank.ex
+++ lib/smart_bank.ex
@@ -4,4 +4,4 @@ defmodule SmartBank do
   and business logic.
 
   Contexts are also responsible for managing your data, regardless
-  if it comes from the databa
+  if it comes from the database, an external API or others.
