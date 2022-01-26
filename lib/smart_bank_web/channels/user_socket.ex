d5448007c8bd86773a3e4e56154fc6c1d0c7c5de
negrong <69174899+negrong@users.noreply.github.com>
2022-01-26 11:44:11 +0000

    version 0.3

--- lib/smart_bank_web/channels/user_socket.ex
+++ lib/smart_bank_web/channels/user_socket.ex
@@ -15,4 +15,5 @@ defmodule SmartBankWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket, _co
+  def connect(_params, socket, _connect_info) do
+    {:ok, s
