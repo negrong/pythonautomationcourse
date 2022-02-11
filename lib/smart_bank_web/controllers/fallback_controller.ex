0afce0db13ecbc2474c0877d1bbb200124fa7a5b
negrong <69174899+negrong@users.noreply.github.com>
2022-02-11 21:22:55 +0000

    Release 2.6.1

--- lib/smart_bank_web/controllers/fallback_controller.ex
+++ lib/smart_bank_web/controllers/fallback_controller.ex
@@ -5,3 +5,19 @@ defmodule SmartBankWeb.FallbackController do
 
   See `Phoenix.Controller.action_fallback/1` for more details.
   """
+  use SmartBankWeb, :controller
+
+  def call(conn, {:error, message, code}) do
+    conn
+    |> put_status(code)
+    |> put_view(SmartBankWeb.ErrorView)
+    |> render("#{code}.json", error: %{message: message})
+  end
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(SmartBankWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+end
