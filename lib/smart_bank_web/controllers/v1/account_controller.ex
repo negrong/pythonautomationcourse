73aab47085d8b2f479f7551e344d455f94330912
negrong <69174899+negrong@users.noreply.github.com>
2022-02-13 21:57:56 +0000

    floats

--- lib/smart_bank_web/controllers/v1/account_controller.ex
+++ lib/smart_bank_web/controllers/v1/account_controller.ex
@@ -4,4 +4,6 @@ defmodule SmartBankWeb.V1.AccountController do
   alias SmartBank.Bank
   alias SmartBank.Bank.Account
 
-  action_fallback SmartBankWeb.FallbackCont
+  action_fallback SmartBankWeb.FallbackController
+
+  def index(conn, _params) 
