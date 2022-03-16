5983c0ca972d4b745ad30f6c0d2adf103003786b
negrong <69174899+negrong@users.noreply.github.com>
2022-03-16 08:09:57 +0000

    Translated by zdenek Přidáno: 0, Smazáno: 0, Změněno: 0 (Konflikty: 0)

--- lib/smart_bank_web/controllers/v1/transaction_controller.ex
+++ lib/smart_bank_web/controllers/v1/transaction_controller.ex
@@ -51,4 +51,4 @@ defmodule SmartBankWeb.V1.TransactionController do
     user_api_account = conn.assigns.current_user.account
 
     with true <- user_api_account.id == account_id,
-         {:ok, accou
+         {:ok, account} <- account_id |> Bank.get_accou
