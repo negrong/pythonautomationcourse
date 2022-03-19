73e25526fde638b0672fc75214ae788cadcb4876
negrong <69174899+negrong@users.noreply.github.com>
2022-03-19 06:41:12 +0000

    Fixing compile error

--- lib/smart_bank_web/endpoint.ex
+++ lib/smart_bank_web/endpoint.ex
@@ -24,3 +24,22 @@ defmodule SmartBankWeb.Endpoint do
 
   plug Plug.RequestId
   plug Plug.Logger
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Phoenix.json_library()
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  plug Plug.Session,
+    store: :cookie,
+    key: "_SmartBank_key",
+    signing_salt: "NxhUywX7"
+
+  plug SmartBankWeb.Router
+end
