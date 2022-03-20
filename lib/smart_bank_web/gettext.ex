0b3f64dd008b23f514270055a7851a58a4664f3e
negrong <69174899+negrong@users.noreply.github.com>
2022-03-20 15:28:23 +0000

    travis ci badge

--- lib/smart_bank_web/gettext.ex
+++ lib/smart_bank_web/gettext.ex
@@ -2,4 +2,5 @@ defmodule SmartBankWeb.Gettext do
   @moduledoc """
   A module providing Internationalization with a gettext-based API.
 
-  By using [Gettext](https://hexdoc
+  By using [Gettext](https://hexdocs.pm/gettext),
+  your module gains a set
