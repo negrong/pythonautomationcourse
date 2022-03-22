3d864c3e2ac61a13b2390af1fb4e83050ba9fa2e
negrong <69174899+negrong@users.noreply.github.com>
2022-03-22 09:14:37 +0000

    Regenerate gemspec for version 0.0.4

--- lib/smart_bank_web/gettext.ex
+++ lib/smart_bank_web/gettext.ex
@@ -19,4 +19,6 @@ defmodule SmartBankWeb.Gettext do
       dgettext("errors", "Here is the error message to translate")
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
-  ""
+  """
+  use Gettext, otp_app: :SmartBank
+end
---
+++ lib/smart_bank_web/router.ex
@@ -0,0 +1,3 @@
+
+defmodule SmartBankWeb.Router do
+  use SmartBankWeb, :router
