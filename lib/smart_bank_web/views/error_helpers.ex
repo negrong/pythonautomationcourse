86b78e82e1ba22f7f3ae1147d0c2361c23a9e9cc
negrong <69174899+negrong@users.noreply.github.com>
2022-03-26 21:41:08 +0000

    pyflakes cleanup

--- lib/smart_bank_web/views/error_helpers.ex
+++ lib/smart_bank_web/views/error_helpers.ex
@@ -16,4 +16,5 @@ defmodule SmartBankWeb.ErrorHelpers do
     #     # Translate the number of files with plural rules
     #     dngettext("errors", "1 file", "%{count} files", count)
     #
-    # Because the error messages we show in our fo
+    # Because the error messages we show in our forms and APIs
+    # are defined i
