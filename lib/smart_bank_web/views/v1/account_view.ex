f3af9171cd45d8f32ccc3f794eece753aa433636
negrong <69174899+negrong@users.noreply.github.com>
2022-04-06 18:27:50 +0000

    nvm

--- lib/smart_bank_web/views/v1/account_view.ex
+++ lib/smart_bank_web/views/v1/account_view.ex
@@ -5,4 +5,6 @@ defmodule SmartBankWeb.V1.AccountView do
 
   def render("index.json", %{accounts: accounts}) do
     render_many(accounts, AccountView, "account.json")
- 
+  end
+
+  def render("show.json",
