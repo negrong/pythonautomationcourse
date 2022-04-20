202991bce609f51ac3ad4caa9c6f568522e108d7
negrong <69174899+negrong@users.noreply.github.com>
2022-04-20 16:51:20 +0000

    [Composer] Add composer definition to each component.

--- lib/smart_bank_web/views/v1/transaction_view.ex
+++ lib/smart_bank_web/views/v1/transaction_view.ex
@@ -35,4 +35,5 @@ defmodule SmartBankWeb.V1.TransactionView do
     %{
       today: render_many(transactions.today, TransactionView, "transaction.json"),
       month: transactions.month |> handle_groups(),
-      y
+      year: transactions.year |> handle_groups()
+    }
