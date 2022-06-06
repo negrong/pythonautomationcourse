33af545a1ab86338e626dfdcbd00767f3a662bbd
negrong <69174899+negrong@users.noreply.github.com>
2022-06-06 16:24:51 +0000

    removed test

--- priv/repo/migrations/20191016221215_enable_extensions.exs
+++ priv/repo/migrations/20191016221215_enable_extensions.exs
@@ -2,4 +2,7 @@ defmodule SmartBank.Repo.Migrations.EnableExtensions do
   use Ecto.Migration
 
   def up do
-    execute "CREATE EXTENSION IF NOT EXISTS \"uuid-os
+    execute "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\" WITH SCHEMA public;"
+  end
+
+  d
