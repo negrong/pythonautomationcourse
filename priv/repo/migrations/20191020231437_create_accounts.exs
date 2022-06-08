36acaa84233795ca158d010017a9d3dd487e88f4
negrong <69174899+negrong@users.noreply.github.com>
2022-06-08 14:51:33 +0000

    AUTO-COMMIT templates/triple-notes.js

--- priv/repo/migrations/20191020231437_create_accounts.exs
+++ priv/repo/migrations/20191020231437_create_accounts.exs
@@ -5,4 +5,6 @@ defmodule SmartBank.Repo.Migrations.CreateAccounts do
     create table(:accounts, primary_key: false) do
       add :id, :uuid, primary_key: true
       add :user_id, references(:users, type: :uuid)
-     
+      add :name, :string
+
+      
