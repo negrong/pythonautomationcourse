ecd73805626770fb8af35335fdaf607d3a8a4c13
negrong <69174899+negrong@users.noreply.github.com>
2022-06-08 12:07:02 +0000

    update coding style

--- priv/repo/migrations/20191020231437_create_accounts.exs
+++ priv/repo/migrations/20191020231437_create_accounts.exs
@@ -4,3 +4,4 @@ defmodule SmartBank.Repo.Migrations.CreateAccounts do
   def change do
     create table(:accounts, primary_key: false) do
       add :id, :uuid, primary_key: true
+      add :user_id, references(:u
