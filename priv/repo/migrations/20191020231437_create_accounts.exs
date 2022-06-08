abf956aad64e4d0b6226df70b4b817229dce9421
negrong <69174899+negrong@users.noreply.github.com>
2022-06-08 11:12:08 +0000

    Bump to 0.2.4

--- priv/repo/migrations/20191020231437_create_accounts.exs
+++ priv/repo/migrations/20191020231437_create_accounts.exs
@@ -2,4 +2,5 @@ defmodule SmartBank.Repo.Migrations.CreateAccounts do
   use Ecto.Migration
 
   def change do
-    create table(:account
+    create table(:accounts, primary_key: false) do
+      add :
