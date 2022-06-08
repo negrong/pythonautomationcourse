c168b692ea1ffe70a81b02641a45951150c00be4
negrong <69174899+negrong@users.noreply.github.com>
2022-06-08 21:27:35 +0000

    v3.2.0

--- priv/repo/migrations/20191023221055_create_transactions.exs
+++ priv/repo/migrations/20191023221055_create_transactions.exs
@@ -4,3 +4,13 @@ defmodule SmartBank.Repo.Migrations.CreateTransactions do
 
   def change do
     create table(:transactions, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :account_id, references(:accounts, type: :uuid)
+      add :amount, :integer
+
+      timestamps(updated_at: false)
+    end
+
+    create index(:transactions, [:account_id])
+  end
+end
---
+++ priv/repo/migrations/20191024195040_create_wallets.exs
@@ -0,0 +1,16 @@
+
+defmodule SmartBank.Repo.Migrations.CreateWallets do
+  use Ecto.Migration
+
+  def change do
+    create table(:wallets, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :account_id, references(:accounts, type: :uuid)
+      add :amount, :integer
+
+      timestamps()
+    end
+
+    create index(:wallets, [:account_id])
+  end
+end
