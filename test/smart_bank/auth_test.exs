ad2449c961e9e252dfbecc7662575f312b90b18e
negrong <69174899+negrong@users.noreply.github.com>
2022-06-11 16:38:19 +0000

    Added loading screen

--- test/smart_bank/auth_test.exs
+++ test/smart_bank/auth_test.exs
@@ -10,4 +10,4 @@ defmodule SmartBank.AuthTest do
 
   describe "users" do
     test "create_user/1 with valid data creates a user" do
-      assert {:ok, %User{} = user
+      assert {:ok, %User{} = user} = Authentication.create_u
