2310938f246b44d20b1ced0694e85c5f234a2fc7
negrong <69174899+negrong@users.noreply.github.com>
2022-06-11 09:58:29 +0000

    Initial impl

--- test/smart_bank/auth_test.exs
+++ test/smart_bank/auth_test.exs
@@ -9,4 +9,4 @@ defmodule SmartBank.AuthTest do
   @valid_user_attrs %{"email" => Faker.Internet.email(), "password" => Faker.String.base64()}
 
   describe "users" do
-    test "create_user/1 wi
+    test "create_user/1 with valid data creates a user"
