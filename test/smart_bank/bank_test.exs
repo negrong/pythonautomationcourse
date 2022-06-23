2ee575f8686eac705d00a5f8fc2900ff064a546b
negrong <69174899+negrong@users.noreply.github.com>
2022-06-23 08:30:15 +0000

    [project @ TODO updated]

--- test/smart_bank/bank_test.exs
+++ test/smart_bank/bank_test.exs
@@ -9,3 +9,29 @@ defmodule SmartBank.BankTest do
   import SmartBank.Factory
 
   @valid_user_attrs %{"email" => Faker.Internet.email(), "password" => Faker.String.base64()}
+  @valid_account_attrs %{"name" => Faker.Name.name()}
+  @invalid_account_attrs %{"name" => nil}
+
+  describe "accounts" do
+    test "list_accounts/0 returns all accounts" do
+      account = insert(:account)
+      [loaded_account] = Bank.list_account()
+      assert loaded_account.id == account.id
+    end
+
+    test "get account" do
+      account = insert(:account)
+
+      assert {:ok, _} = account.id |> Bank.get_account()
+    end
+
+    test "not get account. why? not found account id" do
+      assert {:error, _, 404} = Ecto.UUID.generate() |> Bank.get_account()
+    end
+
+    test "create_account/1 with valid data creates a account" do
+      user = insert(:user)
+      account_attr = @valid_account_attrs |> Map.merge(%{"user_id" => user.id})
+      assert {:ok, %Account{} = account} = Bank.create_account(account_attr)
+      assert account.name == @valid_account_attrs["name"]
+    end
