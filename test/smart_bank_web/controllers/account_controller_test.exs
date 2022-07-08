ab17d0e90c78524b8c310384bd928397e85d64a9
negrong <69174899+negrong@users.noreply.github.com>
2022-07-08 18:04:15 +0000

    removed unused file

--- test/smart_bank_web/controllers/account_controller_test.exs
+++ test/smart_bank_web/controllers/account_controller_test.exs
@@ -32,4 +32,4 @@ defmodule SmartBankWeb.AccountControllerTest do
 
   describe "create" do
     test "create account", %{conn: conn, jwt_account_token: jwt_account_token} do
-      conn = 
+      conn = conn |> put_req_header("author
