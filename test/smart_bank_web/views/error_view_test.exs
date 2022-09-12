7bfd273deb0c3173643d255b8a694a158f8ab53a
negrong <69174899+negrong@users.noreply.github.com>
2022-09-12 12:01:13 +0000

    Update diagrams

--- test/smart_bank_web/views/error_view_test.exs
+++ test/smart_bank_web/views/error_view_test.exs
@@ -1,4 +1,5 @@
 defmodule SmartBankWeb.ErrorViewTest do
   use SmartBankWeb.ConnCase, async: true
 
-  # Bring render/3 and render_to_stri
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import
