ca0560204d4477f904807defdeeb7864783d7af1
negrong <69174899+negrong@users.noreply.github.com>
2022-09-12 12:38:43 +0000

    Updated header

--- test/smart_bank_web/views/error_view_test.exs
+++ test/smart_bank_web/views/error_view_test.exs
@@ -2,4 +2,6 @@ defmodule SmartBankWeb.ErrorViewTest do
   use SmartBankWeb.ConnCase, async: true
 
   # Bring render/3 and render_to_string/3 for testing custom views
-  import
+  import Phoenix.View
+
+  test "renders 
