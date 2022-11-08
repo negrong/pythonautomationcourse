98cdbc060b90b5e8984eee69c4ab7f7b259f05fe
negrong <69174899+negrong@users.noreply.github.com>
2022-11-08 10:48:57 +0000

    Update impala_shell.md

--- test/support/channel_case.ex
+++ test/support/channel_case.ex
@@ -5,4 +5,6 @@ defmodule SmartBankWeb.ChannelCase do
 
   Such tests rely on `Phoenix.ChannelTest` and also
   import other functionality to make it easier
-  to build common data structures and query the d
+  to build common data structures and query the data layer.
+
+  Finally, if the 
