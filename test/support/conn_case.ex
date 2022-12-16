a9faccdba1d5e732e5632e52a7e5cc303d4cc08f
negrong <69174899+negrong@users.noreply.github.com>
2022-12-16 16:46:27 +0000

    Update tagkks.md

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -1,4 +1,6 @@
 defmodule SmartBankWeb.ConnCase do
   @moduledoc """
   This module defines the test case to be used by
-  tests that require se
+  tests that require setting up a connection.
+
+  Such te
