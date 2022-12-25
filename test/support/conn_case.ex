d025bbf42c9fefacec9640b135a19b6daad794cc
negrong <69174899+negrong@users.noreply.github.com>
2022-12-25 08:37:04 +0000

    pep8 corrections

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -10,4 +10,8 @@ defmodule SmartBankWeb.ConnCase do
   Finally, if the test case interacts with the database,
   it cannot be async. For this reason, every test runs
   inside a transaction which is reset at the beginning
-  of the test unless the test case is marked as a
+  of the test unless the test case is marked as async.
+  """
+
+  use ExUnit.CaseTemplate
+ 
