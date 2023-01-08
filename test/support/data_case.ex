eda73467b165dd17886dc0684158313e9ee99843
negrong <69174899+negrong@users.noreply.github.com>
2023-01-08 08:33:35 +0000

    more work in progress

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -2,3 +2,5 @@ defmodule SmartBank.DataCase do
   @moduledoc """
   This module defines the setup for tests requiring
   access to the application's data layer.
+
+  You may define functions here to be use
