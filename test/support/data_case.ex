911cb1e523134111de936aadd8d71ac3eb8179dd
negrong <69174899+negrong@users.noreply.github.com>
2023-01-30 09:19:13 +0000

    Use dist install by default

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -9,4 +9,7 @@ defmodule SmartBank.DataCase do
   Finally, if the test case interacts with the database,
   it cannot be async. For this reason, every test runs
   inside a transaction which is reset at the beginning
-  of the test unless the test case is marked as a
+  of the test unless the test case is marked as async.
+  """
+
+  use ExUnit.CaseTemplate
