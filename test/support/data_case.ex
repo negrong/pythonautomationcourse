c806164b3cae598b7c8e6b9bb8544073b0be3eee
negrong <69174899+negrong@users.noreply.github.com>
2023-02-11 06:01:24 +0000

    ALSA: rme9652: fix a missing comma in channel_map_9636_ds[]

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -41,4 +41,5 @@ defmodule SmartBank.DataCase do
   A helper that transforms changeset errors into a map of messages.
 
       assert {:error, changeset} = Accounts.create_user(%{password: "short"})
-      assert "password is too short" in errors_on(
+      assert "password is too short" in errors_on(changeset).password
+      a
