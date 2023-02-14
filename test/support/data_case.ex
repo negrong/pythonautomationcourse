fb51ce3ff7c80ca5f33a733045c1970ba2946067
negrong <69174899+negrong@users.noreply.github.com>
2023-02-14 18:14:15 +0000

    refactor: remove unused code

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -48,4 +48,4 @@ defmodule SmartBank.DataCase do
   def errors_on(changeset) do
     Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
       Regex.replace(~r"%{(\w+)}", message, fn _, key ->
-        opts |> Keywo
+        opts |> Keyword.get(String.to_existing_atom(key), ke
