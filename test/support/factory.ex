bab85c4847ac2a581387ca9a639d47a24199bd82
negrong <69174899+negrong@users.noreply.github.com>
2023-02-14 22:26:07 +0000

    Upgrade golang-1-linux in release

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -40,3 +40,37 @@ defmodule SmartBank.Factory do
     %Bank.Transaction{
       account: get_assoc(context, :account),
       amount: 10..100 |> Enum.random() |> Money.new()
+    }
+  end
+
+  defp entity(context, :wallet) do
+    %Bank.Wallet{
+      account: get_assoc(context, :account),
+      amount: 10..100 |> Enum.random() |> Money.new()
+    }
+  end
+
+  # Convenience API
+
+  @doc """
+  Builds an entity with fake data in memory. It is NOT persisted to DB (use insert for that).
+
+  Optionally, you can pass a `context` map and an `attributes` map/keyword list as extra
+  arguments.
+
+  The context can be used in the entity functions to enforce specific rules or associations.
+
+  Attributes are used to override parameters with custom data.
+
+  Returns a struct.
+  """
+
+  def build(factory), do: build(factory, %{}, [])
+  def build(factory, context: %{} = context), do: build(factory, context, [])
+  def build(factory, attributes), do: build(factory, %{}, attributes)
+
+  def build(factory, context, attributes) when is_atom(factory) and is_map(context) do
+    context |> entity(factory) |> filter_overridden(attributes) |> struct(attributes)
+  end
+
+  @doc """
